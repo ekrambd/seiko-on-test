e5775e43851d314f7d9a58e7a7ee88f3cb4930a8
ekrambd <ekramhossainekram28@gmail.com>
2026-05-08 20:01:48 +0600

    upd

--- query.php
+++ query.php
@@ -9,6 +9,19 @@ $conn = mysql_connect($host, $user, $pass);
 if (!$conn) {
     die("DB connection failed: " . mysql_error());
 }
-echo "connected";
 mysql_select_db($db, $conn);
+
+// run query
+$query = "SELECT * FROM admin_log";
+$result = mysql_query($query);
+
+if (!$result) {
+    die("Query Error: " . mysql_error());
+}
+
+// print data
+while ($row = mysql_fetch_assoc($result)) {
+    print_r($row);
+    echo "<br><br>";
+}
 ?>
